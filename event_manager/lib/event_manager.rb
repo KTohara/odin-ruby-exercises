3e7bba05111ba9c76501e684d4a44dd3dcd1c1df
kenta <ktohara@gmail.com>
2022-06-04 14:04:12 -0400

    Add valid_zip method

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,8 +1,25 @@
 require 'csv'
+
+def valid_zip(zipcode)
+  case
+  when zipcode.nil? then '00000'
+  when zipcode.length > 5 then zipcode[0...5]
+  when zipcode.length < 5 then zipcode.rjust(5, '0')
+  else zipcode
+  end
+end
+
 puts 'EventManager initialized.'
 
-contents = CSV.open('event_attendees.csv', headers: true)
+contents = CSV.open(
+  '../event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
 contents.each do |row|
-  name = row[2]
-  puts name
+  name = row[:first_name]
+  zipcode = valid_zip(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
