4537a3a485dbe21bf71f8566819bdd40147de05c
kenta <ktohara@gmail.com>
2022-06-27 21:59:52 -0400

    Finish caesar_spec

--- caesar_cipher/lib/caesar.rb
+++ caesar_cipher/lib/caesar.rb
@@ -7,14 +7,16 @@
 # find the index of current character, check for case, add the number of shifts, and modulo by 26 to make sure it wraps around
 # if the ciphered character is an uppercase, inject as uppercase, else inject as lowercase
 
-def caesar_cipher(str, shift = 0) 
-  abc = ("a".."z").to_a
-  str.split("").inject([]) do |cipher, char| 
-    next cipher << char unless abc.include?(char.downcase)
+def caesar_cipher(str, shift = 0)
+  abc = ('a'..'z').to_a
+
+  str.split('').inject([]) do |shifted, char|
+    next shifted << char unless abc.include?(char.downcase)
+
     new_index = (abc.index(char.downcase) + shift) % abc.length
-    char == char.upcase ? cipher << abc[new_index].upcase : cipher << abc[new_index]
+    shifted << (char == char.upcase ? abc[new_index].upcase : abc[new_index])
   end.join
 end
 
-p caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
 #  "Bmfy f xywnsl!"
--- caesar_cipher/spec/caesar_spec.rb
+++ caesar_cipher/spec/caesar_spec.rb
@@ -1,2 +1,23 @@
 require 'caesar'
 
+describe '#caesar_cipher' do
+  it 'should shift each character by the given number in the alphabet' do
+    expect(caesar_cipher('what a string', 5)).to eql('bmfy f xywnsl')
+  end
+
+  it 'should shift correctly with numbers greater than 26' do
+    expect(caesar_cipher('hello', 50)).to eql('fcjjm')
+  end
+
+  it 'should not shift any non-alphabetical characters' do
+    expect(caesar_cipher('!@#123', 5)).to eql('!@#123')
+  end
+
+  it 'should shift capitalized letters' do
+    expect(caesar_cipher('Hello World', 10)).to eql('Rovvy Gybvn')
+  end
+
+  it 'should shift negative numbers' do
+    expect(caesar_cipher('Hello World!', -52)).to eql('Hello World!')
+  end
+end
